3916c242aa0d425cd2fb0042d1a559027fdad185
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-29 12:16:16 +0530

    Setting up client

---
+++ client/src/component/CustomButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomButton = () => {
+  return (
+    <div>
+      CustomButton
+    </div>
+  )
+}
+
+export default CustomButton
--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link , useNavigate } from 'react-router-dom'
+
+
+import { CustomButton } from './'
+import { logo, menu, serach, thirdweb } from '../assets'
+import { navlinks } from '../constants'
 
 const Navbar = () => {
+  const navigate = useNavigate()
+  const [isActive, setIsActive] = useState('dashboard')
+  const [toggleDrawer, setToggleDrawer] = useState(false)
   return (
     <div>
       This is Navbar
