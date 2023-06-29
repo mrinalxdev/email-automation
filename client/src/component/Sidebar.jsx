5928d1dc080b27345abecbd3273d628dfda24523
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-29 11:11:22 +0530

    Done : Sidebar of the project

--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -5,15 +5,21 @@ import { navlinks } from "../constants";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div
-    className={`w-[48] h-[48px] rounded-[20px] ${
+    className={`w-[48px] h-[48px] rounded-[10px] ${
       isActive && isActive === name && "bg-[#2c2f32]"
-    } flex justify-center items-center ${!disabled && "cursor-pointer"} ${styles}`} onClick={handleClick}
+    } flex justify-center items-center ${
+      !disabled && "cursor-pointer"
+    } ${styles}`}
+    onClick={handleClick}
   >
-    {isActive ? (
+    {!isActive ? (
       <img src={imgUrl} className="w-1/2 h-1/2" />
     ) : (
-      <img src={imgUrl} className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
-    ) }
+      <img
+        src={imgUrl}
+        className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+      />
+    )}
   </div>
 );
 
@@ -27,6 +33,25 @@ const Sidebar = () => {
       <Link to="/">
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
       </Link>
+      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-10">
+        <div className="flex flex-col justify-center items-center gap-3 ">
+          {navlinks.map((link) => (
+            <Icon
+              key={link.name}
+              {...link}
+              isActive={isActive}
+              handleClick={() => {
+                if (!link.disabled) {
+                  setIsActive(link.name);
+                  navigate(link.link);
+                }
+              }}
+            />
+          ))}
+        </div>
+
+        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+      </div>
     </div>
   );
 };
