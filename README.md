8141366ba8d3bd717180c5b9af1c90c8a5ffd4b7
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-08-25 02:32:38 +0530

    Added New data

--- README.md
+++
@@ -1,2 +0,0 @@
-# CrowFund
-A web3 crowdfunding app for the project founders who want donations in crypto currency and make something which will impact the world
--- client/src/component/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Sidebar } from './Sidebar'
-export { default as Navbar } from './Navbar'
-export { default as CustomButton } from './CustomButton'
